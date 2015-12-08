b7c37050df01338a8d7a7d667a49dfc35088a789
iBullRay <bullray097@yandex.ru>
2015-12-08 16:23:33 +0200

    libhardware: Added alpha to hwc_layer
    Signed-off-by: iBullRay <bullray097@yandex.ru>

--- include/hardware/hwcomposer_v0.h
+++ include/hardware/hwcomposer_v0.h
@@ -103,6 +103,11 @@ typedef struct hwc_layer {
             /* blending to apply during composition */
             int32_t blending;
 
+            /* for HWC_BLENDING_NONE, alpha is not used.
+             * for HWC_BLENDING_PREMULT and HWC_BLENDING_COVERAGE, it is plane alpha value.
+             * for HWC_BLENDING_DIM, it is the alpha in source color (0,0,0,alpha). */
+            int32_t alpha;
+
             /* area of the source to consider, the origin is the top-left corner of
              * the buffer */
             hwc_rect_t sourceCrop;
